e1fed9ad792ece49afe9ff6be0f6211ac8407fe9
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-04-11 15:48:27 +0300

    new projects

---
+++ API/Models/AppUser.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace API.Models;
+
+public class AppUser
+{
+
+}
---
+++ API/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
