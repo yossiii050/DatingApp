156e27ed39abce68c204a71a4692321bafed6334
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-04-11 22:16:47 +0300

    login Endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,6 +32,25 @@ public class AccountController(DataContext context):BaseApiController
         return user;
     }   
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user=await context.Users.FirstOrDefaultAsync(u=>u.UserName== loginDto.UserName.ToLower());
+        if(user==null)
+            return Unauthorized("Invalid username or password");
+        
+        using var hmac= new HMACSHA512(user.PasswordSalt);
+        var computedHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if(computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Invalid username or password");
+        }
+
+        return user;
+
+    }
 
     private async Task<bool> UserExists(string UserName)
     {
