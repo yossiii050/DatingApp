4ff8dd7b878c0ba45546576ced9258acc24d0c29
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-06-07 17:26:14 +0300

    creating repository pattern

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using API.Models;
+
+namespace API.Interfaces;
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+    Task<bool> SaveAllAsync();
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+    Task<AppUser?> GetUserByIdAsync(int id);
+    Task<AppUser?> GetUserByUsernamesync(string username);
+}
