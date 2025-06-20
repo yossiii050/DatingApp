8fd23935a2c60ab8c4dbe0cc9a379f65951e82a2
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-06-20 18:51:48 +0300

    add member service

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +8,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  http=inject(HttpClient);
+export class HomeComponent  {
   registerMode=false;
-  users: any;
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
-  
   registerToggle(){
     this.registerMode=!this.registerMode
   }
@@ -26,11 +19,5 @@ export class HomeComponent implements OnInit {
     this.registerMode=event;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/user').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request has completed")
-    })
-  }
+
 }
