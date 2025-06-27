7ee35d0465c5bef8ff641731d6465926a729c95d
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-06-27 21:14:39 +0300

    Using the service to store state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,10 +19,18 @@ export class MembersService {
   }
 
   getMember(username: string){
+    const member=this.members().find(x=>x.userName==username);
+    if(member !== undefined) return of(member);
+
     return this.http.get<Member>(this.baseUrl+'user/'+username);
   }
 
   updateMember(member:Member){
-    return this.http.put(this.baseUrl+'user',member);
+    return this.http.put(this.baseUrl+'user',member).pipe(
+      tap(()=> {
+        this.members.update(members=>members.map(m=> m.userName === member.userName 
+          ? member: m))
+      })
+    )
   }
 }
